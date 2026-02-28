b4588be8feee984ea3145e9a8168e93634124a7a
vishenka759 <666vishenka@gmail.com>
2026-02-28 12:46:49 +0400

    Deleted all files except README

--- app/src/main/java/com/example/flightsearch/domain/model/Airport.kt
+++
@@ -1,9 +0,0 @@
-package com.example.flightsearch.domain.model
-
-data class Airport(
-    val id: Int,
-    val iataCode: String,
-    val name: String,
-    val passengers: Int,
-)
-
--- app/src/main/java/com/example/flightsearch/domain/model/Flight.kt
+++
@@ -1,9 +0,0 @@
-package com.example.flightsearch.domain.model
-
-data class Flight(
-    val id: Long,
-    val departure: Airport,
-    val destination: Airport,
-    val isFavorite: Boolean,
-)
-
